67c957cc84a70b693b31c99a7bf8be91b24f11da
szvaff <pnagy90@gmail.com>
2020-03-16 23:30:58 +0100

    added basic skeleton

---
+++ src/app/build/build.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bd-build',
+  templateUrl: './build.component.html',
+  styleUrls: ['./build.component.scss']
+})
+export class BuildComponent {
+  title = 'bitrise-dashboard';
+}
