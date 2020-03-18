43d64758d462ae40bd578a662bde4f342fb0518e
szvaff <pnagy90@gmail.com>
2020-03-18 17:47:16 +0100

    added ngx-translate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,17 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialModule } from './material.module';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { BuildListComponent } from './build/build-list.component';
 import { httpInterceptorProviders } from './shared/interceptors';
 import { BuildComponent } from './build/components/build.component';
 import { SpinnerComponent } from './shared/components/spinner.component';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -25,7 +31,15 @@ import { SpinnerComponent } from './shared/components/spinner.component';
     BrowserAnimationsModule,
     MaterialModule,
     HttpClientModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'en',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [
     httpInterceptorProviders
--- src/app/shared/interceptors/base-url-interceptor.ts
+++ src/app/shared/interceptors/base-url-interceptor.ts
@@ -5,6 +5,10 @@ import { Observable } from 'rxjs';
 @Injectable()
 export class BaseUrlInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (req.url.includes('assets')) {
+      return next.handle(req);
+    }
+
     const apiReq = req.clone({ url: `api/${req.url}` });
     return next.handle(apiReq);
   }
