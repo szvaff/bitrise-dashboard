a0afe57752bcfecf19801318692356ec5637dac8
szvaff <pnagy90@gmail.com>
2020-03-19 22:40:09 +0100

    added basic e2e tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,16 +1,60 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { browser, logging, by, ExpectedConditions } from 'protractor';
 
-describe('workspace-project App', () => {
+describe('Bitrise Dashboard', () => {
   let page: AppPage;
 
-  beforeEach(() => {
+  beforeAll(() => {
     page = new AppPage();
+    page.navigateTo();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('bitrise-dashboard app is running!');
+  it('should display', () => {
+    expect(page.getRoot().isPresent()).toBeTruthy();
+  });
+
+  it('should display app title', () => {
+    expect(page.getAppTitle()).toEqual('Bitrise Dashboard');
+  });
+
+  it('should display organization selector', () => {
+    expect(page.getOrganizationSelect().isPresent()).toBeTruthy();
+  });
+
+  it('should display build list', () => {
+    expect(page.getBuildList().isPresent()).toBeTruthy();
+  });
+
+  it('should display app list', () => {
+    expect(page.getAppList().isPresent()).toBeTruthy();
+  });
+
+  it('should display icon for builds', () => {
+    expect(page.getBuildIcons().isPresent()).toBeTruthy();
+  });
+
+  it('should display app title for builds', () => {
+    expect(page.getBuildAppTitles().isPresent()).toBeTruthy();
+  });
+
+  it('should display build number for builds', () => {
+    expect(page.getBuildBuildNumbers().isPresent()).toBeTruthy();
+  });
+
+  it('should display owner for builds', () => {
+    expect(page.getBuildOwners().isPresent()).toBeTruthy();
+  });
+
+  it('should display trigger date of builds', () => {
+    expect(page.getTriggerDates().isPresent()).toBeTruthy();
+  });
+
+  it('should display name for apps', () => {
+    expect(page.getAppAppTitles().isPresent()).toBeTruthy();
+  });
+
+  it('should display owner for apps', () => {
+    expect(page.getAppOwners().isPresent()).toBeTruthy();
   });
 
   afterEach(async () => {
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,63 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ElementFinder, Locator } from 'protractor';
 
 export class AppPage {
   navigateTo(): Promise<unknown> {
     return browser.get(browser.baseUrl) as Promise<unknown>;
   }
 
-  getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+  getRoot(): ElementFinder {
+    return element(by.css('bd-root'));
+  }
+
+  getAppTitle(): Promise<string> {
+    return element(by.css('bd-header span:first-child')).getText() as Promise<string>;
+  }
+
+  getOrganizationSelect(): ElementFinder {
+    return element(by.css('bd-header mat-select'));
+  }
+
+  getBuildList(): ElementFinder {
+    return element(by.css('.build-list-wrapper'));
+  }
+
+  getAppList(): ElementFinder {
+    return element(by.css('.app-list-wrapper'));
+  }
+
+  getBuildListSpinner(): ElementFinder {
+    return element(by.css('.build-list-wrapper bd-spinner'));
+  }
+
+  getAppListSpinner(): ElementFinder {
+    return element(by.css('.app-list-wrapper bd-spinner'));
+  }
+
+  getBuildIcons(): ElementFinder {
+    return element(by.css('.build-list-wrapper .bd-build .mat-icon'));
+  }
+
+  getBuildAppTitles(): ElementFinder {
+    return element(by.css('.build-list-wrapper .bd-build .app-title'));
+  }
+
+  getBuildBuildNumbers(): ElementFinder {
+    return element(by.css('.build-list-wrapper .bd-build .build-number'));
+  }
+
+  getBuildOwners(): ElementFinder {
+    return element(by.css('.build-list-wrapper .bd-build .owner'));
+  }
+
+  getTriggerDates(): ElementFinder {
+    return element(by.css('.build-list-wrapper .bd-build .triggered'));
+  }
+
+  getAppAppTitles(): ElementFinder {
+    return element(by.css('.app-list-wrapper .bd-user-app .app-title'));
+  }
+
+  getAppOwners(): ElementFinder {
+    return element(by.css('.app-list-wrapper .bd-user-app .owner'));
   }
 }
